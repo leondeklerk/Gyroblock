20993e63c78f58ad78dd38bff37a45fe189063ec
Leon de Klerk <developer@leondeklerk.com>
2020-10-27 20:41:23 +0100

    Migrate to AndroidX and bump targetSDK
    Signed-off-by: Leon de Klerk <developer@leondeklerk.com>

--- app/src/main/java/nl/leontheclerk/gyroblock/SettingsActivity.java
+++ app/src/main/java/nl/leontheclerk/gyroblock/SettingsActivity.java
@@ -1,7 +1,7 @@
 package nl.leontheclerk.gyroblock;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
